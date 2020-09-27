7df9198d14ca84f6554d43025022cdf47483151e
Paul Rutledge <rutledgepaulv@gmail.com>
2020-09-26 22:49:25 -0500

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/piped "0.1.6"
+(defproject org.clojars.rutledgepaulv/piped "0.1.7-SNAPSHOT"
 
   :description
   "A library for processing AWS SQS messages from a Clojure application."
