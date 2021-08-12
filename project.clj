9cdc156a9a6795b46248d3782a7e189c601c4631
Paul Rutledge <rutledgepaulv@gmail.com>
2021-08-11 22:24:38 -0500

    upgrade dependencies (#8)

--- project.clj
+++ project.clj
@@ -25,12 +25,12 @@
    ["snapshots" :clojars]]
 
   :dependencies
-  [[org.clojure/clojure "1.10.1"]
-   [com.cognitect.aws/api "0.8.474"]
-   [com.cognitect.aws/endpoints "1.1.11.842"]
-   [com.cognitect.aws/sqs "801.2.702.0"]
-   [org.clojars.rutledgepaulv/aws-api-credential-providers "0.1.0"]
-   [org.clojure/core.async "1.3.610"]
+  [[org.clojure/clojure "1.10.3"]
+   [com.cognitect.aws/api "0.8.515"]
+   [com.cognitect.aws/endpoints "1.1.12.42"]
+   [com.cognitect.aws/sqs "811.2.958.0"]
+   [org.clojars.rutledgepaulv/aws-api-credential-providers "0.1.1"]
+   [org.clojure/core.async "1.3.618"]
    [org.clojure/tools.logging "1.1.0"]]
 
   :plugins
@@ -39,8 +39,8 @@
   :profiles
   {:test
    {:dependencies
-    [[org.testcontainers/testcontainers "1.14.3" :scope "test"]
-     [org.slf4j/slf4j-simple "1.7.30" :scope "test"]]}}
+    [[org.testcontainers/testcontainers "1.16.0" :scope "test"]
+     [org.slf4j/slf4j-simple "1.7.32" :scope "test"]]}}
 
   :repl-options
   {:init-ns piped.core})
