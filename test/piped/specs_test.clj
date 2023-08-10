5371940be8ce077372f86860d64d7bc8071c9211
Aaron Walker <aaron@treasuryprime.com>
2023-08-10 00:24:49 -0400

    add tests for spec

---
+++ test/piped/specs_test.clj
@@ -0,0 +1,32 @@
+(ns piped.specs-test
+  (:require
+   [clojure.spec.alpha :as s]
+   [clojure.test :refer [deftest is testing]]))
+
+(deftest specs-test
+  (testing "processor required fields"
+    (let [spec {:queue-url "http://queue-url"
+                :consumer-fn (fn [msg]
+                               (constantly msg)
+                               :ack)}]
+      (is (s/valid? :piped/options-map spec))
+      (is (not (s/valid? :piped/options-map (spec dissoc :queue-url))))
+      (is (not (s/valid? :piped/options-map (spec dissoc :consumer-fn))))))
+  (testing "action maps"
+    (let [spec {:action :ack
+                :delay-seconds 5}]
+      (is (s/valid? :piped/action-map spec))
+      (is (not (s/valid? :piped/action-map
+                         (assoc spec
+                                :delay-seconds "a"))))
+      (is (not (s/valid? :piped/action-map
+                         (assoc spec
+                                :delay-seconds -1))))
+      (is (not (s/valid? :piped/action-map
+                         (assoc spec
+                                :delay-seconds 1.13))))
+      (is (not (s/valid? :piped/action-map
+                         (assoc spec
+                                :action :do-something))))
+      (is (s/valid? :piped/action-map (assoc spec
+                                             :action :nack))))))
